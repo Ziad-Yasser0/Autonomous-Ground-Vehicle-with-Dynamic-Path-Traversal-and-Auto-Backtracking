43c2fd70489df6e59954c2b77e260030c8de1b36
SeifMohamed7599 <swaginohio420@gmail.com>
2026-09-05 22:51:35 +0300

    changes in DCMOTOR_program.c functions

--- DCMOTOR_cfg.h
+++ DCMOTOR_cfg.h
@@ -1,10 +1,10 @@
 #ifndef DCMOTOR_CFG_H_
 #define DCMOTOR_CFG_H_
 
-#define DCMOTOR_u8_PORT               DIO_u8PORTA
+//#define DCMOTOR_u8_PORT               DIO_u8PORTA
 
-#define DCMOTOR_u8_PIN1               DIO_u8PIN0
-#define DCMOTOR_u8_PIN2               DIO_u8PIN1
+//#define DCMOTOR_u8_PIN1               DIO_u8PIN0
+//#define DCMOTOR_u8_PIN2               DIO_u8PIN1
 
 
 #endif
--- DCMOTOR_program.c
+++ DCMOTOR_program.c
@@ -1,40 +1,39 @@
 #include "BIT_MATH.h"
 #include "STD_TYPES.h"
 #include "defines.h"
-
 #include "DIO_interfce.h"
-
+#include "CAR_cfg.h"
 #include "DCMOTOR_cfg.h"
 #include "DCMOTOR_private.h"
 #include "DCMOTOR_interface.h"
 
 
-void DCMOTOR_voidInit(void)
+void DCMOTOR_voidInit(uint8 Copy_u8_MOTORPIN0,uint8 Copy_u8_MOTORPIN1)
 {
-	DIO_u8SetPinDirection(DCMOTOR_u8_PORT,DCMOTOR_u8_PIN1,DIO_u8PIN_OUTPUT);
-	DIO_u8SetPinDirection(DCMOTOR_u8_PORT,DCMOTOR_u8_PIN2,DIO_u8PIN_OUTPUT);
+	DIO_u8SetPinDirection(CAR_u8WHEELSPORT,Copy_u8_MOTORPIN0,DIO_u8PIN_OUTPUT);
+	DIO_u8SetPinDirection(CAR_u8WHEELSPORT,Copy_u8_MOTORPIN1,DIO_u8PIN_OUTPUT);
 }
 
-void DCMOTOR_voidMove(uint8 Copy_u8Direction)
+void DCMOTOR_voidMove(uint8 Copy_u8Direction,uint8 Copy_u8_MOTORPIN0,uint8 Copy_u8_MOTORPIN1)
 {
 	if(Copy_u8Direction == DCMOTOR_u8_CW)
 	{
-		DIO_u8SetPinValue(DCMOTOR_u8_PORT,DCMOTOR_u8_PIN2,DIO_u8PIN_LOW);
-		DIO_u8SetPinValue(DCMOTOR_u8_PORT,DCMOTOR_u8_PIN1,DIO_u8PIN_HIGH);
+		DIO_u8SetPinValue(CAR_u8WHEELSPORT,Copy_u8_MOTORPIN1,DIO_u8PIN_LOW);
+		DIO_u8SetPinValue(CAR_u8WHEELSPORT,Copy_u8_MOTORPIN0,DIO_u8PIN_HIGH);
 		
 	}
 	else if(Copy_u8Direction == DCMOTOR_u8_CCW)
 	{
-		DIO_u8SetPinValue(DCMOTOR_u8_PORT,DCMOTOR_u8_PIN1,DIO_u8PIN_LOW);
-		DIO_u8SetPinValue(DCMOTOR_u8_PORT,DCMOTOR_u8_PIN2,DIO_u8PIN_HIGH);
+		DIO_u8SetPinValue(CAR_u8WHEELSPORT,Copy_u8_MOTORPIN0,DIO_u8PIN_LOW);
+		DIO_u8SetPinValue(CAR_u8WHEELSPORT,Copy_u8_MOTORPIN1,DIO_u8PIN_HIGH);
 	}
 }
 
 
-void DCMOTOR_voidStop(void)
+void DCMOTOR_voidStop(uint8 Copy_u8_MOTORPIN0,uint8 Copy_u8_MOTORPIN1)
 {
-	DIO_u8SetPinValue(DCMOTOR_u8_PORT,DCMOTOR_u8_PIN1,DIO_u8PIN_LOW);
-	DIO_u8SetPinValue(DCMOTOR_u8_PORT,DCMOTOR_u8_PIN2,DIO_u8PIN_LOW);
+	DIO_u8SetPinValue(CAR_u8WHEELSPORT,Copy_u8_MOTORPIN0,DIO_u8PIN_LOW);
+	DIO_u8SetPinValue(CAR_u8WHEELSPORT,Copy_u8_MOTORPIN1,DIO_u8PIN_LOW);
 }
 
 

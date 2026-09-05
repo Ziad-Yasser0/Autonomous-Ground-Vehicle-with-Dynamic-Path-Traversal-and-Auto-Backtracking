74aa01f9e83066f0ca47d3769d32c99a96670a17
Al-Raies <saybar2aman24@gmail.com>
2026-09-05 10:28:57 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 5, 2026
+ *      Author: sayba
+ */
+
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "DIO_Interfce.h"
+#include "DIO_Cfg.h"
+#include "DCMOTOR_cfg.h"
+#include "CAR_interface.h"
+#include "CAR_cfg.h"
+
+
+int main(){
+
+}
